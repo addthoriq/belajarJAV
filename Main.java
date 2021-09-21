d56b5dc637f4b3500270abbe4f8ecb28e5c7f402
addthoriq <alvarro.thoriq@gmail.com>
2021-09-21 10:45:00 +0800

    firs init

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+    nama file dan nama class utama harus sama
+*/
+
+import javax.swing.JOptionPane;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        // JOptionPane.showMessageDialog(null, "Hello World");
+        // JOptionPane.showMessageDialog(null, "Gomennasai", "Gagal", JOptionPane.ERROR_MESSAGE); 
+    }
+}
